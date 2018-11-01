3f7cfdf647dcf7183f9f184b2e3a04f013f33b1f
tharindu <tharindu@WSO2s-MacBook-Pro.local>
2018-11-01 11:18:39 +0530

    Remove unwanted files

--- 01-systems-with-heterogenous-msg-format/01.1-soap-to-json/1.1.1-soap-to-json-w-PLF/run-scenario.sh
+++ 01-systems-with-heterogenous-msg-format/01.1-soap-to-json/1.1.1-soap-to-json-w-PLF/run-scenario.sh
@@ -4,12 +4,11 @@ repo='https://github.com/sashikamw/iam-scenario-tests.git'
 TEST_DIR='iam-scenario-tests'
 DIR=$2
 export DATA_BUCKET_LOCATION=$DIR
-FILE1=${DIR}/infrastructure.properties
-FILE2=${DIR}/testplan-props.properties
-
 
 git clone $repo
 cd $TEST_DIR
 mvn clean install
 
+echo 'Copying surefire-reports .....'
 cp -r modules/integration/tests-integration/tests-backend/target/surefire-reports ${DIR}
+ls $DIR
